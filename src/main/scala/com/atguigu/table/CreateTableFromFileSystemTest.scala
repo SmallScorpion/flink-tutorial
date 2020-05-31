124cbaebe261449102f0586339949114782ff148
SmallScorpion <EvildoerScorpion@outlook.com>
2020-05-31 20:32:57 +0800

    flink

--- src/main/scala/com/atguigu/table/CreateTableFromFileSystemTest.scala
+++ src/main/scala/com/atguigu/table/CreateTableFromFileSystemTest.scala
@@ -15,24 +15,41 @@ object CreateTableFromFileSystemTest {
     val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
 
     // --------------------- 读取文件数据 ---------------------------
-    val filePath = "D:\\MyWork\\WorkSpaceIDEA\\flink-tutorial\\src\\main\\resources\\SensorReading.txt"
+    val fileInputPath = "D:\\MyWork\\WorkSpaceIDEA\\flink-tutorial\\src\\main\\resources\\SensorReading.txt"
 
-    tableEnv.connect( new FileSystem().path(filePath) ) // 定义表的数据来源，和外部系统建立连接
+    tableEnv.connect( new FileSystem().path(fileInputPath) ) // 定义表的数据来源，和外部系统建立连接
       .withFormat( new OldCsv() ) // 定义从外部文件读取数据之后的格式化方法
       .withSchema( new Schema() // 定义表结构
           .field("id", DataTypes.STRING())
           .field("timestamp", DataTypes.BIGINT())
           .field("temperature", DataTypes.DOUBLE())
       )
-      .createTemporaryTable( "inputTable" ) // 在表环境中注册一张表(创建)
+      .createTemporaryTable( "fileInputTable" ) // 在表环境中注册一张表(创建)
 
 
-    // 测试输出
-    val sensorTable: Table = tableEnv.from( "inputTable" )
+    // 查询
+    val sensorTable: Table = tableEnv.from( "fileInputTable" )
     val resultTable: Table = sensorTable
         .select('id, 'temperature) // 查询id和temperature字段
         .filter('id === "sensor_1") // 输出sensor_1得数据
-    resultTable.toAppendStream[ (String, Double) ].print( "FileSystem" )
+
+
+    // 输出到文件的表结构定义
+    val fileOutPath = "D:\\MyWork\\WorkSpaceIDEA\\flink-tutorial\\src\\main\\resources\\SensorReadingOutput.txt"
+
+    tableEnv.connect( new FileSystem().path(fileOutPath) )
+      .withFormat( new OldCsv() )
+      .withSchema( new Schema()
+        // 字段要与查询中select()中的字段一致
+        .field("id", DataTypes.STRING())
+        .field("temperature", DataTypes.DOUBLE())
+      )
+      .createTemporaryTable( "fileOutputTable" )
+
+
+    // 将查询结果输出到目标表
+    resultTable.insertInto("fileOutputTable")
+
 
 
     env.execute(" table connect fileSystem test job")
---
+++ src/main/scala/com/atguigu/table/DataStreamToTableTest.scala
@@ -0,0 +1,51 @@
+package com.atguigu.table
+
+import com.atguigu.bean.SensorReading
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.{DataTypes, Table}
+import org.apache.flink.table.api.scala._
+
+object DataStreamToTableTest {
+  def main(args: Array[String]): Unit = {
+
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    // 创建表环境
+    val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
+
+    val inputDStream: DataStream[String] = env.readTextFile("D:\\MyWork\\WorkSpaceIDEA\\flink-tutorial\\src\\main\\resources\\SensorReading.txt")
+
+    val dataDstream: DataStream[SensorReading] = inputDStream.map(
+      data => {
+        val dataArray: Array[String] = data.split(",")
+        SensorReading(dataArray(0), dataArray(1).toLong, dataArray(2).toDouble)
+      })
+
+
+
+    // 常规创建
+    val dataTable: Table = tableEnv.fromDataStream(dataDstream)
+    // Schema基于名称
+    val sensorTable = tableEnv.fromDataStream(dataDstream,
+      'timestamp as 'ts, 'id as 'myId, 'temperature)
+    // Schema基于位置 一一对应
+    val sensorTable_lo = tableEnv.fromDataStream(dataDstream, 'myId, 'ts)
+
+
+    // ------------------------  创建视图 -----------------
+    tableEnv.createTemporaryView("sensorView", dataDstream)
+    tableEnv.createTemporaryView("sensorView", dataDstream, 'id, 'temperature, 'timestamp as 'ts)
+    tableEnv.createTemporaryView("sensorView", sensorTable)
+
+    // 测试输出
+    val resultTable: Table = dataTable
+      .select('id, 'temperature) // 查询id和temperature字段
+      .filter('id === "sensor_1") // 输出sensor_1得数据
+
+    resultTable.toAppendStream[ (String, Double) ].print( "data" )
+
+
+    env.execute(" table DS test job")
+  }
+}
